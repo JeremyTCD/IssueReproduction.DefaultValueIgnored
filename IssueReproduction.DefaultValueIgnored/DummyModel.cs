7d8f78260b73b102b45ee669dc083d7b69fd1b18
JeremyTCD <jeremytanchongdao@gmail.com>
2019-01-17 22:04:16 +0800

    Initial commit

---
+++ IssueReproduction.DefaultValueIgnored/DummyModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace IssueReproduction.DefaultValueIgnored
+{
+    public class DummyModel
+    {
+        public DummyModel(int dummyProperty1, int dummyProperty2)
+        {
+            if (dummyProperty2 != 1)
+            {
+                throw new Exception("Expected value: 1, Received value: " + dummyProperty2);
+            }
+        }
+
+        public int DummyProperty1 { get; }
+
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate), DefaultValue(1)]
+        public int DummyProperty2 { get; }
+    }
+}
---
+++ IssueReproduction.DefaultValueIgnored/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace IssueReproduction.DefaultValueIgnored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"{}";
+
+            while (true)
+            {
+                var jsonSerializer = new JsonSerializer() { ContractResolver = new DefaultContractResolver() };
+                var threads = new Thread[2];
+                Exception exception = null;
+
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    var thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            using (var jsonTextReader = new JsonTextReader(new StringReader(json)))
+                            {
+                                DummyModel result = jsonSerializer.Deserialize<DummyModel>(jsonTextReader);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            exception = e;
+                        }
+                    });
+                    threads[i] = thread;
+                }
+
+                threads[0].Start();
+                threads[1].Start();
+
+                threads[0].Join();
+                threads[1].Join();
+
+                if (exception != null)
+                {
+                    Console.WriteLine(exception.ToString());
+                    break;
+                }
+            }
+        }
+    }
+}
